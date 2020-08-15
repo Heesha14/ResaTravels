b6529fcd8ea16a21ef41143b25682d0eb59c97e1
User <nadun2952@gmail.com>
2020-08-15 18:53:32 +0530

    All Hotel Veiw / Nadun

---
+++ app/src/main/java/com/example/resatravels/All_Hotel_Views.java
@@ -0,0 +1,14 @@
+package com.example.resatravels;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class All_Hotel_Views extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_all__hotel__views);
+    }
+}
--- app/src/main/java/com/example/resatravels/MainActivity.java
+++ app/src/main/java/com/example/resatravels/MainActivity.java
@@ -3,12 +3,25 @@ package com.example.resatravels;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-
+import android.widget.Button;
+import android.content.Intent;
+import android.view.View;
 public class MainActivity extends AppCompatActivity {
-
+    public Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = (Button)findViewById(R.id.button1);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent= new Intent(MainActivity.this, All_Hotel_Views.class);
+                startActivity(intent);
+            }
+
+
+        });
     }
 }
