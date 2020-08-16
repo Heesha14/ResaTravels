36cca3facfbed6aa239fb51a576f3cb48a0e67f0
Heesha14 <57593483+Heesha14@users.noreply.github.com>
2020-08-16 23:26:36 +0530

    Booking Confirmed dialog box added/Heesha

--- app/src/main/java/com/example/resatravels/Heesha_User_Add_Booking.java
+++ app/src/main/java/com/example/resatravels/Heesha_User_Add_Booking.java
@@ -9,22 +9,35 @@ import android.widget.Button;
 
 public class Heesha_User_Add_Booking extends AppCompatActivity {
 
+    public void User_Cancel_Booking_Display_Vehicles(View view){
+        Intent intentc = new Intent(this, Heesha_User_Display_Vehicles.class);
+        startActivity(intentc);
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_heesha__user__add__booking);
-        Button btn_booking_vehicle = findViewById(R.id.h_user_book_cancel);
-        btn_booking_vehicle.setOnClickListener(new View.OnClickListener() {
+        Button btn_book_vehicle = findViewById(R.id.h_user_book_confirm);
+        btn_book_vehicle.setOnClickListener(new View.OnClickListener(){
+
             @Override
             public void onClick(View view) {
-                openHeesha_User_Cancel_Booking_Display_Vehicles();
+                openDialog();
             }
-
         });
+    }
+
+    private void openDialog() {
+        Heesha_Booking_Confirm_Pop_Up dialogPopUp = new Heesha_Booking_Confirm_Pop_Up();
+        dialogPopUp.show(getSupportFragmentManager(),"example dialog");
     }
 
     private void openHeesha_User_Cancel_Booking_Display_Vehicles() {
         Intent intent= new Intent(this, Heesha_User_Display_Vehicles.class);
         startActivity(intent);
     }
+
+
+
 }
