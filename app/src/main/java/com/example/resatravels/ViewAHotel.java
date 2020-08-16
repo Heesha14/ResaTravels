6da700dfeafb58f1c2c717722c4a3e96a7e11c44
User <nadun2952@gmail.com>
2020-08-16 13:35:53 +0530

    Login 3 / Nadun

--- app/src/main/java/com/example/resatravels/ViewAHotel.java
+++ app/src/main/java/com/example/resatravels/ViewAHotel.java
@@ -8,19 +8,17 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class All_Hotel_Views extends AppCompatActivity {
-
-    TextView textView;
+public class ViewAHotel extends AppCompatActivity {
+    public Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_all__hotel__views);
-
-        textView = (TextView)findViewById(R.id.NtextView);
-        textView.setOnClickListener(new View.OnClickListener() {
+        setContentView(R.layout.activity_view_a_hotel);
+        button = (Button) findViewById(R.id.button3);
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent= new Intent(All_Hotel_Views.this, ViewAHotel.class);
+                Intent intent= new Intent(ViewAHotel.this, Login.class);
                 startActivity(intent);
             }
 
